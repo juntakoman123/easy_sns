71657232a2d71e1ff6203988a2df632a83a78a82
anko tubura <juntakoman123@gmail.com>
2019-06-19 23:38:25 +0900

    create posts_table

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  root to: "home#top" #ルートはhomeコントローラーのtopアクションに設定する
   get 'home/top'
   get 'home/about'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+
+
 end
