6be1eb4c1e5d5cef5aa9262d3ee220c9eba5417e
anko tubura <juntakoman123@gmail.com>
2019-06-20 02:13:53 +0900

    create User

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+
+
+
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  
+
+
+
+
+end
